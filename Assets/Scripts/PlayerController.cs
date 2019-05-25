21fad41deac63eb25300d40366aa2737705c1bba
lestherreynoso <lhreynos@gmail.com>
2019-05-25 12:18:34 -0400

    Basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5;
-    public float jumpSpeed = 10; 
+    public float jumpSpeed = 10;
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    float accelerationTimeGrounded = .1f;
+    float accelerationTimeAirborne = .2f;
+    float smoothing;
     float xInput;
     bool jump;
 
@@ -28,12 +34,25 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 movement = new Vector2(xInput * moveSpeed * Time.deltaTime, body.velocity.y);
+        Vector2 movement = new Vector2(xInput * moveSpeed * Time.deltaTime, 0);
 
+        float targetVelocityX = xInput * moveSpeed * Time.deltaTime;
+        //Vector2 movement  = Vector2.right * Mathf.SmoothDamp(body.velocity.x, targetVelocityX, ref smoothing, accelerationTimeGrounded );
+
+        body.velocity += movement;
         Debug.Log(movement);
-        if(jump)
+        if (jump)
         {
-            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+            body.velocity = Vector2.up * jumpSpeed;
+        }
+
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
