8b7b12627cc2ce619566b4cfeb7125604cb751d7
lestherreynoso <lhreynos@gmail.com>
2019-05-25 13:51:27 -0400

    Dotween Example

---
+++ Assets/Scripts/TweenTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            transform.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, 30), .1f).From(Quaternion.Euler(0, 0, -30)).SetLoops(4, LoopType.Yoyo);
+        }
+    }
+}
