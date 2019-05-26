9e815cfb8b99d844c9cdb4ef8da4530bf2f68b9b
lestherreynoso <lhreynos@gmail.com>
2019-05-26 14:10:15 -0400

    fail condition sign

--- Assets/Scripts/OutOfOrderSign.cs
+++ Assets/Scripts/OutOfOrderSign.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using Rewired;
 using System;
 using Cinemachine;
+
+using DG.Tweening;
+
 public class OutOfOrderSign : MonoBehaviour
 {
     public GameObject keyboardGameObject, controllerGameObject;
@@ -12,8 +15,10 @@ public class OutOfOrderSign : MonoBehaviour
     private int playerId = 0;
     private Player player; // The Rewired Player
     private Rigidbody2D body;
-
-
+    private GameObject activeObject;
+    private bool signHidden;
+    private Vector3 originalPosition;
+    private bool gameEnded;
 
     // Start is called before the first frame update
     void Awake()
@@ -24,11 +29,14 @@ public class OutOfOrderSign : MonoBehaviour
         ReInput.ControllerDisconnectedEvent += HandleControllerDisconnect;
         keyboardGameObject.SetActive(ReInput.controllers.Joysticks.Count == 0);
         controllerGameObject.SetActive(ReInput.controllers.Joysticks.Count != 0);
+        activeObject = keyboardGameObject.activeInHierarchy ? keyboardGameObject : controllerGameObject;
 
         vCam = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
         if (vCam == null) Debug.LogError("No Cam found");
 
         PlayerObject = GameObject.Find("Player").transform;
+
+        originalPosition = transform.position;
     }
 
     private void HandleControllerConnect(ControllerStatusChangedEventArgs obj)
@@ -37,7 +45,8 @@ public class OutOfOrderSign : MonoBehaviour
 
         keyboardGameObject.SetActive(obj.controllerType != ControllerType.Joystick);
         controllerGameObject.SetActive(obj.controllerType == ControllerType.Joystick);
-        
+
+        activeObject = keyboardGameObject.activeInHierarchy ? keyboardGameObject : controllerGameObject;
     }
 
     private void HandleControllerDisconnect(ControllerStatusChangedEventArgs obj)
@@ -46,6 +55,8 @@ public class OutOfOrderSign : MonoBehaviour
 
         keyboardGameObject.SetActive(obj.controllerType == ControllerType.Joystick);
         controllerGameObject.SetActive(obj.controllerType != ControllerType.Joystick);
+
+        activeObject = keyboardGameObject.activeInHierarchy ? keyboardGameObject : controllerGameObject;
 
     }
 
@@ -57,17 +68,35 @@ public class OutOfOrderSign : MonoBehaviour
             body.simulated = true;
         }
 
-        if (vCam.transform.position.y == PlayerObject.position.y ||
-            vCam.transform.position.y < PlayerObject.position.y) 
+        if ((vCam.transform.position.y == PlayerObject.position.y ||
+            vCam.transform.position.y < PlayerObject.position.y) && !signHidden) 
         {
             Invoke("HideSign", 3f);
             vCam.Follow = PlayerObject;
+        }
+
+        if(player.GetButtonDown("Fail"))
+        {
+            Fail();
         }
     }
 
     private void HideSign()
     {
         body.simulated = false;
-        gameObject.SetActive(false);
+        activeObject.SetActive(false);
+        signHidden = true;
+        CancelInvoke("HideSign");
+    }
+
+    public void Fail()
+    {
+        body.simulated = false;
+        body.velocity = Vector2.zero;
+        transform.position = originalPosition;
+        transform.DOScale(1f, 1f).From(5);
+        vCam.Follow = transform;
+        activeObject.SetActive(true);
+        gameEnded = true;
     }
 }
